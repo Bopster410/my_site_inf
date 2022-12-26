15c1f43c8e0c9db0624189cd488e7ba521ff803d
Сергей Дранцев <serg.dr2004@gmail.com>
2022-12-26 10:28:38 +0300

    added db

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///proj.db'
 app.config['SECRET_KEY'] = '76ab62945617c423e8b122cfffb9ac1a'
+db = SQLAlchemy(app)
 
 from project import routes
--- project/routes.py
+++ project/routes.py
@@ -1,10 +1,16 @@
-from project import app
+from project import app, db
 from project.forms import RegistrationForm
+from project.models import User
 from flask import render_template
 
-@app.route('/reg')
+@app.route('/reg', methods=['GET', 'POST'])
 def registration():
     form = RegistrationForm()
+    username = form.username.data
+    password = form.password.data
+    if username:
+        db.session.add(User(username=username,  password=password))
+        db.session.commit()
     return render_template('registration.html', form=form)
 
 @app.route('/')
