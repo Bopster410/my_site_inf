b6c97131aacfcaa117a2e026fa735afad4743d35
Bopster411 <serg.dr2004@gmail.com>
2022-12-27 14:04:54 +0300

    added bcrypt for passwords

--- project/__init__.py
+++ project/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///proj.db'
 app.config['SECRET_KEY'] = '76ab62945617c423e8b122cfffb9ac1a'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt()
 login_manager = LoginManager(app)
 
 from project import routes
--- project/routes.py
+++ project/routes.py
@@ -1,4 +1,4 @@
-from project import app, db
+from project import app, db, bcrypt
 from project.forms import RegistrationForm, LogInForm
 from project.models import User
 from flask import render_template, redirect, url_for
@@ -9,7 +9,7 @@ def registration():
     form = RegistrationForm()
     if form.validate_on_submit():
         username = form.username.data
-        password = form.password.data # TODO add bcrypt
+        password = bcrypt.generate_password_hash(form.password.data) # TODO add bcrypt
         email = form.email.data
         db.session.add(User(username=username,  password=password, email=email))
         db.session.commit()
@@ -21,7 +21,7 @@ def log_in():
     form = LogInForm()
     if form.validate_on_submit():
         user = db.session.execute(db.select(User).where(User.email == form.email.data)).scalars().first()
-        if user and form.password.data == user.password: #TODO bcrypt also
+        if user and  bcrypt.check_password_hash(user.password, form.password.data): #TODO bcrypt also
             login_user(user) #TODO remember me 
             return redirect(url_for('main_page'))
     return render_template('log_in.html', form=form)
