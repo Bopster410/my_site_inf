12ade9392e32715d4725caedc032a7abc77c6b54
Bopster411 <serg.dr2004@gmail.com>
2022-12-29 09:50:59 +0300

    Added validators

--- project/forms.py
+++ project/forms.py
@@ -1,5 +1,5 @@
 from project import db
-from project.models import User
+from project.models import User, Product
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, EmailField, BooleanField, SubmitField, ValidationError, IntegerField
@@ -70,7 +70,21 @@ class AddProductForm(FlaskForm):
     image = FileField('Product picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Add')
 
+    def validate_title(form, field):
+        exists = db.session.execute(db.select(Product).where(Product.title == field.data)).scalars().first() != None
+        if exists:
+            raise ValidationError('This title is already exists')
+
+    def validate_price(form, field):
+        if field.data <= 0:
+            raise ValidationError('Price must be greater than 0')
+
 
 class DeleteProductForm(FlaskForm):
-    title_to_delete = StringField('ID', validators=[DataRequired(), Length(min=6, max=20)])
+    title_to_delete = StringField('Title', validators=[DataRequired(), Length(min=6, max=20)])
     delete = SubmitField('Delete')
+
+    def validate_title_to_delete(form, field):
+        exists = db.session.execute(db.select(Product).where(Product.title == field.data)).scalars().first() != None
+        if not exists:
+            raise ValidationError("This title doesn't exist")
